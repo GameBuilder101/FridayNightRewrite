eeec85e01dc6204997cefb8c18ca30a1a27e8a04
GameBuilder101 <56798450+GameBuilder101@users.noreply.github.com>
2022-11-02 22:20:03 -0400

    Asset stuff

---
+++ source/Album.hx
@@ -0,0 +1,16 @@
+package;
+
+import assets.Registry;
+
+typedef AlbumData =
+{
+	name:String
+}
+
+class AlbumRegistry extends Registry<AlbumData>
+{
+	function loadData(path:String):AlbumData
+	{
+		return null;
+	}
+}
--- source/Main.hx
+++ source/Main.hx
@@ -5,12 +5,9 @@ import openfl.display.Sprite;
 
 class Main extends Sprite
 {
-	public static var assetManager(default, null):AssetManager;
-
 	public function new()
 	{
 		super();
-		assetManager = new AssetManager();
 		addChild(new FlxGame(0, 0, PlayState, 1.0, 60, 60, true, true));
 	}
 }
